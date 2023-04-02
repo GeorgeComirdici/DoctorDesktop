6c5be8fd627f3f060a0de15a04a77a798f47a78c
GEORGE ADRIAN COMIRDICI <george.comirdici@s.unibuc.ro>
2023-04-02 19:26:41 +0300

    Error handling, Error responses

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var something = _context.Products.Find(422);
+            if(something == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+            return Ok();
+        }
+
+          [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var something = _context.Products.Find(422);
+            var somethingToReturn = something.ToString();
+            return Ok();
+        }
+
+          [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+          [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
